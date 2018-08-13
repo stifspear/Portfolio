5fe4c4d0bef36c62daca7b889ca7e4e9c027c71f
Stif Subba <stifsubb@amazon.com>
2018-08-13 21:41:53 +0530

    Added all the data

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataModel } from '../datamodel';
+
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutmeComponent implements OnInit {
 
+  dataObj: any;
+  selectedValues: any;
   constructor() { }
 
   ngOnInit() {
+    this.dataObj = DataModel.data;
+    console.log(this.dataObj);
+    this.selectedValues = {
+      'experiences': this.getKeys(this.dataObj['experiences'])[0],
+      'projects': this.getKeys(this.dataObj['projects'])[0]
+    };
+  }
+
+  /**
+   * Function will update the description based on the selected element
+   * @param section name of the element clicked on
+   * @param itemElement of the section
+   */
+  updateDesc(index: number, itemElement: string) {
+    this.selectedValues[index] = itemElement;
+  }
+
+  getKeys(obj: Object): Array<any> {
+    return Object.keys(obj);
   }
 
 }
---
+++ src/app/datamodel.ts
@@ -0,0 +1,13 @@
+export class DataModel {
+    static data: Object = {
+        'experiences': {
+        'Experience one': 'Some exprience about the company experience one',
+        'Experience Two': 'Some experienec about the company two',
+        'Experience Three': 'Some experienec about the company three'
+    },
+        'projects': {
+        'Project 1': 'some project i worked on other than work',
+        'Project 2': 'some project i worked on other than school'
+        }
+};
+}
