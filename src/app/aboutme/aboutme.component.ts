45a0185b7da906056747e65311bb4e3ac32d772f
Stif Subba <stifsubb@amazon.com>
2018-08-20 22:01:56 +0530

    Final changes. Now only have to add styles

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -9,17 +9,26 @@ import { DataModel } from '../datamodel';
 })
 export class AboutmeComponent implements OnInit {
 
-  dataObj: any;
+  contactDetails: any;
+  dataModel: any;
+  education: any;
+  experiences: any;
+  projects: any;
   selectedValues: any;
+  skills: any;
   constructor() { }
 
   ngOnInit() {
-    this.dataObj = DataModel.data;
-    console.log(this.dataObj);
+    this.contactDetails = DataModel.contactDetails;
+    this.dataModel = DataModel;
+    this.education = DataModel.education;
+    this.experiences = DataModel.experiences;
+    this.projects = DataModel.projects;
     this.selectedValues = {
-      'experiences': this.getKeys(this.dataObj['experiences'])[0],
-      'projects': this.getKeys(this.dataObj['projects'])[0]
+      'exp': this.experiences[0].desc,
+      'pro': this.projects[0].desc
     };
+    this.skills = DataModel.skills;
   }
 
   /**
@@ -27,8 +36,8 @@ export class AboutmeComponent implements OnInit {
    * @param section name of the element clicked on
    * @param itemElement of the section
    */
-  updateDesc(index: number, itemElement: string) {
-    this.selectedValues[index] = itemElement;
+  updateValue(obj: string, data: string) {
+    this.selectedValues[obj] = data;
   }
 
   getKeys(obj: Object): Array<any> {
--- src/app/datamodel.ts
+++ src/app/datamodel.ts
@@ -1,13 +1,52 @@
 export class DataModel {
-    static data: Object = {
-        'experiences': {
-        'Experience one': 'Some exprience about the company experience one',
-        'Experience Two': 'Some experienec about the company two',
-        'Experience Three': 'Some experienec about the company three'
-    },
-        'projects': {
-        'Project 1': 'some project i worked on other than work',
-        'Project 2': 'some project i worked on other than school'
+    static fullName = 'Stif Spear Subba';
+    static title = 'Software Developer';
+    static aboutMe = 'sdfsdf sdfsd skj sdji k wjl owklk wlk jlkl weoijk jowejsldk';
+    static contactDetails: Object = [
+        {
+            'data': 'Hyderabad, India',
+            'class': 'home-img'
+        },
+        {
+            'data': '+91 7536993270',
+            'class': 'phone-img'
+        },
+        {
+            'data': 'stif_spear123@yahoo.com',
+            'class': 'email-img'
         }
-};
+    ];
+    static education: Object = [
+        'College with some gpa',
+        'School with some percentage',
+        'Some other school with percentage'
+    ];
+    static skills: Object = [
+        'Skills 1',
+        'Skills 2',
+        'Skills 3',
+        'Skills 4',
+        'Skills 5',
+        'Skills 6'
+    ];
+    static experiences: Object = [
+        {
+            'companyName': 'Company',
+            'desc': 'Some exprience about the company experience one',
+        },
+        {
+            'companyName': 'Company 2',
+            'desc': 'Some experienec about the company two'
+        }
+    ];
+    static projects: Object = [
+        {
+            'projectName': 'Project 1',
+            'desc': 'some project i worked on other than work'
+        },
+        {
+            'projectName': 'Project 2',
+            'desc': 'some project i worked on other than school'
+        }
+    ];
 }
